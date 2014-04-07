e17174b266e3c4f8ba4a99fa4c16ad0314d724b1
FND <fnd@localhost.localdomain>
2014-04-07 12:15:04 +0200

    added jQuery dependency
    note that this is installed via Bower
    
    also note that the bundle now also includes jQuery, which significantly
    increases compilation time

--- commonjs/util.js
+++ commonjs/util.js
@@ -1,5 +1,8 @@
 var moment = require("moment");
+var $ = require("./bower_components/jquery/dist/jquery.js");
 
 exports.info = function(msg) {
-	console.log("INFO:", msg, moment().format("MMMM Do YYYY, h:mm:ss a"));
+	var timestamp = moment().format("MMMM Do YYYY, h:mm:ss a");
+	console.log("INFO:", msg, timestamp);
+	$("<p />").text(timestamp).appendTo(document.body);
 };
